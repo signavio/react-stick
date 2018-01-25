633b8ddf715343f567de32266e52110b8061ded5
Jan-Felix <janfelixschwarz@gmail.com>
2018-01-25 18:10:12 +0100

    added demo

---
+++ demo/src/PositionAlignOverview.js
@@ -0,0 +1,188 @@
+import React, { Component } from 'react'
+import { compact } from 'lodash'
+
+import Stick from '../../src'
+
+const formPairs = (listA: Array<string>, listB: Array<string>) =>
+  compact(
+    listA.map((a: string) =>
+      listB.map((b: string) => {
+        if (a === b) {
+          return null
+        }
+
+        return `${a} ${b}`
+      })
+    )
+  )
+
+const positionGroups = formPairs(
+  ['top', 'middle', 'bottom'],
+  ['left', 'center', 'right']
+)
+const alignmentGroups = formPairs(
+  ['top', 'middle', 'bottom'],
+  ['left', 'center', 'right']
+)
+
+const Anchor = () => (
+  <div
+    style={{
+      width: 35,
+      height: 35,
+      backgroundColor: 'rgb(24, 170, 177)',
+    }}
+  />
+)
+
+const Node = () => (
+  <div
+    style={{
+      width: 10,
+      height: 10,
+      backgroundColor: '#ae0d5c',
+    }}
+  />
+)
+
+class PositionAlignOverview extends Component {
+  state = {
+    inline: false,
+    updateOnAnimationFrame: false,
+  }
+
+  render() {
+    const { updateOnAnimationFrame, inline } = this.state
+
+    return (
+      <div>
+        <p>
+          The table shows all combinations of possible values for the{' '}
+          <code>position</code> and <code>align</code> props. The{' '}
+          <code>node</code> elements are colors in red while the anchor elements
+          (<code>children</code>) are colored in blue.
+        </p>
+        <div>
+          <input
+            type="checkbox"
+            value={inline}
+            onChange={() => {
+              this.setState({ inline: !inline })
+            }}
+          />
+          <code>inline</code>
+        </div>
+        <div>
+          <input
+            type="checkbox"
+            value={updateOnAnimationFrame}
+            onChange={() => {
+              this.setState({ updateOnAnimationFrame: !updateOnAnimationFrame })
+            }}
+          />
+          <code>updateOnAnimationFrame</code>
+        </div>
+        <div>
+          <textarea
+            readOnly
+            defaultValue="resize me to check smoothness of Stick"
+          />
+        </div>
+        <table
+          style={{
+            textAlign: 'center',
+            borderCollapse: 'collapse',
+          }}
+        >
+          <tbody
+            style={{
+              borderTop: '1px solid black',
+              borderRight: '1px solid black',
+            }}
+          >
+            {positionGroups.map((positions: Array<string>, i: number) => (
+              <tr key={i}>
+                {positions.map((position: string) => (
+                  <td
+                    key={position}
+                    style={{
+                      padding: '0 10px 10px 10px',
+                      borderLeft: '1px solid black',
+                      borderBottom: '1px solid black',
+                      fontSize: 11,
+                    }}
+                  >
+                    <p>
+                      <pre>position="{position}"</pre>
+                    </p>
+                    <div style={{ display: 'inline-block' }}>
+                      <Stick
+                        inline={inline}
+                        updateOnAnimationFrame={updateOnAnimationFrame}
+                        position={position}
+                        node={<Node />}
+                      >
+                        <Anchor />
+                      </Stick>
+                    </div>
+
+                    <table
+                      style={{
+                        borderCollapse: 'collapse',
+                        marginTop: 20,
+                      }}
+                    >
+                      <tbody
+                        style={{
+                          borderTop: '1px solid gray',
+                          borderRight: '1px solid gray',
+                        }}
+                      >
+                        {alignmentGroups.map(
+                          (alignments: Array<string>, j: number) => (
+                            <tr key={j}>
+                              {alignments.map((alignment: string) => (
+                                <td
+                                  key={alignment}
+                                  style={{
+                                    padding: '0 10px 10px 10px',
+                                    borderLeft: '1px solid gray',
+                                    borderBottom: '1px solid gray',
+                                    fontSize: 11,
+                                  }}
+                                >
+                                  <p>
+                                    <pre>align="{alignment}"</pre>
+                                  </p>
+                                  <div style={{ display: 'inline-block' }}>
+                                    <Stick
+                                      position={position}
+                                      align={alignment}
+                                      inline={inline}
+                                      updateOnAnimationFrame={
+                                        updateOnAnimationFrame
+                                      }
+                                      node={<Node />}
+                                    >
+                                      <Anchor />
+                                    </Stick>
+                                  </div>
+                                </td>
+                              ))}
+                            </tr>
+                          )
+                        )}
+                      </tbody>
+                    </table>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+export default PositionAlignOverview
--- demo/src/index.js
+++ demo/src/index.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
-import Stick from '../../src'
+import PositionAlignOverview from './PositionAlignOverview'
 
 class Demo extends Component {
   render() {
     return (
       <div>
         <h1>react-stick Demo</h1>
-        <Stick />
+        <p>
+          Open{' '}
+          <a href="https://github.com/signavio/react-stick">
+            documention on Github
+          </a>
+        </p>
+        <PositionAlignOverview />
       </div>
     )
   }
