495443fca832310da147d4cedd88e4fa7ce88f26
Jan-Felix <janfelixschwarz@gmail.com>
2018-01-25 15:27:41 +0100

    fixed gp-pages publish

--- scripts/gh-pages-publish.js
+++ scripts/gh-pages-publish.js
@@ -4,15 +4,28 @@ const fs = require('fs')
 
 const branchName = process.argv[2]
 const dir = path.resolve(__dirname, '..', 'demo', 'dist')
+const url = `https://signavio.github.io/react-stick/${branchName}`
 
-if (!fs.existsSync(path)) {
+if (!fs.existsSync(dir)) {
   throw new Error(`${dir} does not exist. Run \`yarn build\` first.`)
 }
 
-console.log(
-  `Publishing demo/dist to https://signavio.github.io/react-stick/${branchName}`
-)
+console.log(`Publishing demo/dist to ${url}...`)
 
-// ghpages.publish('docs', err => {
-//   console.error(err)
-// })
+ghpages.publish(
+  dir,
+  {
+    dest: branchName,
+    message: `Published demo build for \`${branchName}\` branch.`,
+  },
+  err => {
+    if (err) {
+      throw err
+    }
+
+    console.log(
+      '\x1b[32m%s\x1b[0m',
+      `✓ Demo page successfully published to ${url}`
+    )
+  }
+)
