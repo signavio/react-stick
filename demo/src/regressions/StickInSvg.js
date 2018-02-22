7a92d22b19ac4f9b057087727b31d3038a9aa7db
Jan-Felix <janfelixschwarz@gmail.com>
2018-02-22 10:23:34 +0100

    added stick in svg example

---
+++ demo/src/regressions/StickInSvg.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+import Stick from '../../../src'
+
+import Regression from './Regression'
+
+export default function SameWidth() {
+  return (
+    <Regression
+      allBrowsers
+      fixed
+      title="Stick in SVG"
+      description="Stick should also be usable for SVG elements"
+    >
+      <svg width="400" height="200">
+        <Stick
+          component="g"
+          position="top center"
+          node={
+            <div
+              style={{
+                height: 20,
+                width: 20,
+                backgroundColor: '#ae0d5c',
+              }}
+            />
+          }
+        >
+          <ellipse
+            cx="200"
+            cy="100"
+            rx="100"
+            ry="50"
+            style={{ fill: 'rgb(24, 170, 177)' }}
+          />
+        </Stick>
+      </svg>
+    </Regression>
+  )
+}
