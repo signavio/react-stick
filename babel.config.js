87a8f0047e4ba820a4b2cc2c5948927e24316c5d
Philipp Giese <giese.philipp+git@gmail.com>
2020-04-15 18:30:30 +0200

    Improvement/babel setup (#32)
    * bust cache
    
    * simplify circle setup
    
    * more updates for circle
    
    * update nwb
    
    * update lock file
    
    * update babel config
    
    * more babel stuff

--- babel.config.js
+++ babel.config.js
@@ -16,14 +16,8 @@ module.exports = {
   comments: true,
   plugins: [
     '@babel/transform-runtime',
-    '@babel/plugin-transform-flow-strip-types',
     '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-optional-chaining',
 
     'lodash',
-    'syntax-dynamic-import',
-
-    ...(NODE_ENV === 'test' ? ['dynamic-import-node'] : []),
   ],
 }
