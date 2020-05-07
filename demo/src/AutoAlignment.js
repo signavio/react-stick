06d49dedbc977a5d9d8822a9b6e1853aff96c35c
Philipp Giese <giese.philipp+git@gmail.com>
2020-05-07 14:30:11 +0200

    chore: improve auto alignment example

--- demo/src/AutoAlignment.js
+++ demo/src/AutoAlignment.js
@@ -34,28 +34,34 @@ function AutoAlignment() {
 
       <h4>Horizontal flipping</h4>
 
-      <div style={{ height: 350, width: 2000 }}>
-        <Stick
-          autoFlipHorizontally
-          position="middle left"
-          style={{ display: 'inline-block', marginLeft: 250 }}
-          node={
-            <div style={{ backgroundColor: '#ae0d5c', height: 50, width: 200 }}>
-              This is the content of the node
-            </div>
-          }
-        >
-          <div
-            style={{
-              width: 200,
-              height: 100,
-              backgroundColor: 'rgb(24, 170, 177)',
-            }}
+      <div
+        style={{ overflowX: 'auto', border: '1px solid black', padding: 25 }}
+      >
+        <div style={{ height: 350, width: 5000 }}>
+          <Stick
+            autoFlipHorizontally
+            position="middle left"
+            style={{ display: 'inline-block', marginLeft: 250 }}
+            node={
+              <div
+                style={{ backgroundColor: '#ae0d5c', height: 50, width: 200 }}
+              >
+                This is the content of the node
+              </div>
+            }
           >
-            The node of this stick should move to the right if it can't fit to
-            the left
-          </div>
-        </Stick>
+            <div
+              style={{
+                width: 200,
+                height: 100,
+                backgroundColor: 'rgb(24, 170, 177)',
+              }}
+            >
+              The node of this stick should move to the right if it can't fit to
+              the left
+            </div>
+          </Stick>
+        </div>
       </div>
     </div>
   )
