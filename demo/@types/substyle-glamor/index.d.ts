486d0b9c0a0fd5ebedab29415595da4a83609493
Philipp Giese <giese.philipp+git@gmail.com>
2020-10-01 15:04:36 +0200

    remove local type defs

--- demo/@types/substyle-glamor/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'substyle-glamor' {
-  import { Component, ReactNode } from 'react'
-
-  type EnhancerPropsT = {
-    children: ReactNode
-  }
-  export function StylesAsDataAttributes(props: EnhancerPropsT): JSX.Element
-}
--- src/@types/invariant/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'invariant' {
-  function invariant(condition: boolean, message: string): asserts condition
-
-  export = invariant
-}
