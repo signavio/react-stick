c2478f177d9557669dbd4e04c6dec2e540b48b12
AlaeddineMessadi <10847134+AlaeddineMessadi@users.noreply.github.com>
2024-05-16 16:08:21 +0200

    fix(SIGSPM-11659): use element instead

--- src/Stick.tsx
+++ src/Stick.tsx
@@ -99,7 +99,7 @@ function Stick({
 
       const { target } = ev
       if (
-        target instanceof (window.HTMLElement || window.SVGElement) &&
+        target instanceof window.Element &&
         isOutside(anchorRef, containerRef, target)
       ) {
         onClickOutside(ev)
@@ -219,7 +219,7 @@ function Stick({
 function isOutside(
   anchorRef: MutableRefObject<HTMLElement | undefined>,
   containerRef: MutableRefObject<HTMLElement | undefined>,
-  target: HTMLElement | SVGElement
+  target: Element
 ) {
   if (anchorRef.current && anchorRef.current.contains(target)) {
     return false
